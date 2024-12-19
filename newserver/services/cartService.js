72a722d0f74281d04564b9b8719cfd5991fb2112
David Stein <dstein131@gmail.com>
2024-12-19 16:50:13 -0500

    1

--- newserver/services/cartService.js
+++ newserver/services/cartService.js
@@ -3,6 +3,29 @@
 const pool = require('../pool/pool'); // Main DB pool
 
 /**
+ * Ensures that the user has an active cart.
+ * If the user doesn't have a cart, creates one.
+ * @param {number} userId - The ID of the user.
+ * @returns {Promise<number>} - The cart_id of the user's cart.
+ */
+const ensureUserCart = async (userId) => {
+    console.log(`Ensuring cart exists for user ID: ${userId}`);
+
+    // Check if the user already has a cart
+    const [carts] = await pool.query('SELECT cart_id FROM carts WHERE user_id = ?', [userId]);
+    if (carts.length > 0) {
+        console.log(`User ID: ${userId} already has cart ID: ${carts[0].cart_id}`);
+        return carts[0].cart_id;
+    }
+
+    // If no cart exists, create one
+    const [result] = await pool.query('INSERT INTO carts (user_id) VALUES (?)', [userId]);
+    const newCartId = result.insertId;
+    console.log(`Created new cart with ID: ${newCartId} for user ID: ${userId}`);
+    return newCartId;
+};
+
+/**
  * Clears the user's cart by deleting all cart items.
  * Associated addons will be deleted automatically due to ON DELETE CASCADE.
  * @param {number} userId - The ID of the user.
